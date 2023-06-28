2d95976aadfe18392abb9b2959977a261cd4b1bd
amann013 <coolaman4554@gmail.com>
2023-06-28 12:48:00 +0530

    cors added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const path=require('path');
+const cors = require('cors');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
@@ -12,7 +13,7 @@ const bookRouter = require('./routes/api/bookRouter');
 const userRouter = require('./routes/api/userRouter');
 const issueRouter = require('./routes/api/issueRouter');
 const app= express();
-
+app.use(cors()); 
 app.use(function(req, res, next) {
 
   res.header("Access-Control-Allow-Origin", "*");
@@ -42,14 +43,14 @@ app.use('/api/users',userRouter);
 app.use('/api/issues',issueRouter);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 const port = process.env.PORT || 5000;
 
